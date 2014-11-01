02ce751f8cf4e2d68eeaa8b57732950760da9c80
Yuval Cohen <yuval@purplebit.com>
2014-11-01 13:18:35 +0200

    added theme files and sliced index.html, theme is working

---
+++ source/js/google_form.js
@@ -0,0 +1,44 @@
+jQuery(document).ready(function() {
+  // move submit button
+  $('#ss-submit').appendTo('#ss-form');
+  $('#navigation-table').parent().remove();
+
+  //clean up empty tags
+  jQuery('.google-form-wrapper p, .google-form-wrapper br')
+    .filter(function() {
+      return jQuery(this).html() == '';
+    })
+    .remove();
+  
+  //add required class to required elements
+  jQuery('.google-form-wrapper form')
+    .find('.ss-item-required input, .ss-item-required textarea')
+    .filter(function() {
+      return jQuery(this).attr('name').match(/entry\.\d\.single/);
+    })
+    .addClass('required');
+ 
+  //validate the form
+  jQuery('.google-form-wrapper form').validate({
+    submitHandler: function(form) {
+      jQuery(form)
+        .ajaxSubmit({
+          success: function(data) {
+            if (data) {
+              jQuery(form)
+              .hide(200, function() {
+                jQuery(this)
+                  .prev('.success-msg')
+                  .fadeIn('slow')
+              })
+            }
+          },
+          error : function (data) {
+            jQuery('.google-form-wrapper').fadeOut('fast');
+            jQuery('.thank_you_pane').fadeIn('fast');
+            console.error(data);
+          }
+        })
+    }
+  });
+});
